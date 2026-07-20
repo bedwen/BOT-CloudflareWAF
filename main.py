fd1e880b4d8d9bc2595ed7b94e9c8410b3619ce9
bedwen <219533298+bedwen@users.noreply.github.com>
2026-07-20 15:59:50 +0300

    added menu

--- main.py
+++ main.py
@@ -9,21 +9,14 @@ load_dotenv()
 API_TOKEN = os.getenv("CLOUDFLARE_API_TOKEN")
 ZONE_ID = os.getenv("CLOUDFLARE_ZONE_ID")
 
-def main():
-    if not API_TOKEN or not ZONE_ID:
-        print("Error: Please define the CLOUDFLARE_API_TOKEN and CLOUDFLARE_ZONE_ID values in your .env file.")
-        return
-
-    print("Cloudflare WAF Bot Starting...\n")
-
-    #API Client
-    cf_client = CloudflareClient(API_TOKEN, ZONE_ID)
-
+def waf_rules_list(cf_client):
     #WAF rules
     rules = cf_client.get_waf_rules()
+
     if rules:
-        print(f"{len(rules)} Rules")
+        print(f"\n[✔] Success. {len(rules)} Rules \n")
         print("-"*50)
+
         for index, rule in enumerate(rules, start=1):
             rule_id = rule.get("id", "No ID")
             description = rule.get("description", "No Description")
@@ -39,6 +32,67 @@ def main():
         print("\n Connection was established to the API, but no WAF rules were found in this zone.")
     else:
         print("\n No rule was found, or an API connection error occurred.")
+
+
+def block_ip(cf_client):
+    #take ip address from user and block.
+    print("\n"+"-"*50)
+    print("--- IP BLOCK ---")
+
+    target_ip = input("Enter the IP address to block: ")
+    if not target_ip.strip():
+        print("[!] Error: The IP address cannot be left blank. The transaction was canceled.")
+        return
+
+    description = input("Enter a description/note (Press Enter to leave blank): ")
+    if not description.strip():
+        description = "The target IP has been blocked via CLDBOT."
+
+    #try to add rule
+    print(f"\n [/] A rule is being created for the address {target_ip}")
+    block_check = cf_client.block.ip(target_ip(), description)
+
+    if block_ip():
+        print(f"[✔] System: {target_ip} The IP address has been successfully added to the WAF rules.")
+    else:
+        print(f"[X] System: {target_ip} The IP address could not be blocked")
+
+
+def main():
+    if not API_TOKEN or not ZONE_ID:
+        print("[!] Error: “Please define the CLOUDFLARE_API_TOKEN and CLOUDFLARE_ZONE_ID values in your .env file.")
+        return
+
+    print("""
+ ██████╗██╗     ██████╗ ██████╗  ██████╗ ████████╗
+ ██╔════╝██║     ██╔══██╗██╔══██╗██╔═══██╗╚══██╔══╝
+ ██║     ██║     ██║  ██║██████╔╝██║   ██║   ██║   
+ ██║     ██║     ██║  ██║██╔══██╗██║   ██║   ██║   
+ ╚██████╗███████╗██████╔╝██████╔╝╚██████╔╝   ██║   
+  ╚═════╝╚══════╝╚═════╝ ╚═════╝  ╚═════╝    ╚═╝""")
+    print("[/] CLDBOT Starting...")
+    cf_client = CloudflareClient(API_TOKEN, ZONE_ID)
+
+    while True:
+        print("\n" + "="*50)
+        print("CLDBOT MENU")
+        print("="*50+"\n")
+        print("[1] List Rules\n"
+              "[2] Block IP Address\n"
+              "[0] Exit")
+        print("\n" + "="*50)
+
+        choose = input("[>] Please select an operation: ")
+
+        if choose == "1":
+            waf_rules_list(cf_client)
+        elif choose == "2":
+            block_ip(cf_client)
+        elif choose == "0":
+            print("\n[/] Exiting...")
+        else:
+            print("\n[!] Error: Invalid selection. Please select one of the options provided.")
+
 
 if __name__ == "__main__":
     main()
